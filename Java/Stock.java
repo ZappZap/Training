d6bdfdc4bda433b0e29d61a74f099c090a108c3e
ZappZap <panzapris@proton.me>
2025-09-30 18:35:53 +0300

    New Assignment

---
+++ Java/Stock.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; // Import of the Scanner Method.
+import java.util.ArrayList; // Import of the ArrayList Method.
+
+public class Stock {
+    public static Scanner userInput = new Scanner(System.in); // Creates the Scanner Object.
+    private static ArrayList<String> stocks = new ArrayList<>(); // List to store stock items.
+
+    public static void main(String[] args) {
+        
+    }
+}
